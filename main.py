2d910dd9df31cce784316cc72504c32ac013950f
Vadim <crocozybra.v@gmail.com>
2021-09-10 16:54:48 +0300

    First commit with bot, and gitignore

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+import settings
+from typing import Text
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+def greet_user(update, context):
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info('Bot has been started')
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == '__main__':
+    main()
+
